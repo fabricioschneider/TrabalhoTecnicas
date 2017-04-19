0012cd98adbbe76d58acf166c7d79f6b9a15339c
neimardellosbel <neimar.osbel@acad.pucrs.br>
2017-04-18 22:28:27 -0300

    Create saraiva.sql

---
+++ saraiva.sql
@@ -0,0 +1,8 @@
+create table app.products( 
+id integer not null primary key generated always as identity(start with 1, increment by 1),
+name varchar(30) not null,
+description varchar(300) not null,
+image varchar(300) not null,
+edition varchar(30) not null,
+value decimal(10,2);
+)
