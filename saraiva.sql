d148408ae1f6275fac535777bd5705e62e86a1f3
fabricioschneider <fabricioschneider.santos@gmail.com>
2017-04-20 22:22:47 -0300

    Create saraiva.sql
    insert

--- saraiva.sql
+++ saraiva.sql
@@ -5,4 +5,11 @@ description varchar(300) not null,
 image varchar(300) not null,
 edition varchar(30) not null,
 value decimal(10,2)
+
+ insert into app.products ( name, description, image, edition, value)
+ values ('Livro', 'A pedra filosofal', 'foto', 'primeira edição') 
+  
 )
+
+   
+
